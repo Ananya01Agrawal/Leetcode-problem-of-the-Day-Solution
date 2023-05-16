67a565e9f22c932592b24daff4aa84d7910db609
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2023-05-16 18:19:09 +0530

    Create 24. Swap Nodes in Pairs.java

---
+++ 24. Swap Nodes in Pairs.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        // create a dummy node
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+
+        ListNode point=dummy;
+
+        // ensure the nodes we are swapping are not null
+        while(point.next!=null &&point.next.next!=null){
+
+            // identify the nodes to swap
+            ListNode swap1=point.next;
+            ListNode swap2=point.next.next;
+
+            //Actually swap
+            swap1.next=swap2.next;
+            swap2.next=swap1;
+
+            //prepare for teh next
+            point.next=swap2;
+            point=swap1;
+        }
+        // return the start node
+         return dummy.next;
+    }
+}
+        
