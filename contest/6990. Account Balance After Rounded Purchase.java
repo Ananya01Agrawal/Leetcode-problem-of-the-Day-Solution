f5fabe01d8bb66240dbbf916470df18edb7b6ba6
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2023-08-05 21:28:10 +0530

    Create 6990. Account Balance After Rounded Purchase.java

---
+++ contest/6990. Account Balance After Rounded Purchase.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int accountBalanceAfterPurchase(int purchaseAmount) {
+        int n= ((purchaseAmount + 5) / 10) * 10;
+        int dollar=100;
+        int m=(dollar-n);
+        return m;
+                        
+        
+    }
+}
