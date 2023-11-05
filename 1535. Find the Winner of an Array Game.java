453b82760942d5929917dcb38fa431de73a7b2b6
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2023-11-05 09:04:33 +0530

    Create 1535. Find the Winner of an Array Game.java

---
+++ 1535. Find the Winner of an Array Game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int getWinner(int[] arr, int k) {
+        int count=0;
+        int current=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>current){
+                current=arr[i];
+                count=1;
+            }else{
+                count++;
+            }
+            if(count>=k) return current;
+        }
+        return current;
+    }
+}
