79b5bbaebd72bc3c6f6317916154df720d6e103a
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2023-12-30 12:22:02 +0530

    Create 1897. Redistribute Characters to Make All Strings Equal.java

---
+++ 1897. Redistribute Characters to Make All Strings Equal.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean makeEqual(String[] words) {
+        Map<Character, Integer> charCount=new HashMap<>();
+
+        //count the occurences of each character
+        for(String word: words){
+            for(char c:word.toCharArray()){
+                charCount.put(c,charCount.getOrDefault(c,0)+1);
+            }
+        }
+        
+        int n=words.length;
+
+        //check if each character's count is a multiple of the number of strings'
+        for(Map.Entry<Character,Integer> entry:charCount.entrySet()){
+            if(entry.getValue()%n!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
