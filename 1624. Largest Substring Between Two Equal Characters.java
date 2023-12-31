1c5c6fd534fbaaf750579d118ad6167c94a7c400
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2023-12-31 12:42:34 +0530

    Create 1624. Largest Substring Between Two Equal Characters.java

---
+++ 1624. Largest Substring Between Two Equal Characters.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxLengthBetweenEqualCharacters(String s) {
+        // this si the brute force approach to solve thsi problem
+        int current=s.length()-1;
+        for(int i=s.length()-1;i>0;i--){
+            for(int j=0;j+i<s.length();j++){
+                if(s.charAt(j)==s.charAt(j+i)) return i-1;
+            }
+            current--;
+        }
+        return -1;
+    }
+} 
