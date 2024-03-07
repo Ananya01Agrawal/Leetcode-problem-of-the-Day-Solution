28ea83fc39932e48c1ebc46794232c138051a618
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2024-03-07 07:27:32 +0530

    Create 876. Middle of the Linked List.java

---
+++ 876. Middle of the Linked List.java
@@ -0,0 +1,17 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+}
+/*
+Approach: 
+
+Create two pointers slow and fast and initialize them to a head pointer.
+Move slow ptr by one step and simultaneously fast ptr by two steps until fast ptr is NULL or next of fast ptr is NULL.
+When the above condition is met, we can see that the slow ptr is pointing towards the middle of the Linked List and hence we can return the slow pointer.*/
