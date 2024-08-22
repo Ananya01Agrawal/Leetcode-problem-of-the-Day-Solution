511ad987516d61e0a5ffaad323418a65b080a211
Ananya Agrawal <99130567+Ananya01Agrawal@users.noreply.github.com>
2024-08-22 07:40:16 +0530

    Create 476. Number Complement.java

---
+++ 476. Number Complement.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int findComplement(int num) {
+         int n=Integer.toBinaryString(num).length();
+        int h= (1 << n) - 1;
+        return h ^ num;
+        
+    }
+}
